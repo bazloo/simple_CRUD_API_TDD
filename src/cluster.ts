f979252a455788d8d26f1afaae2a3b9830ced313
Roman Panchenko <roman.p@aquimarketing.com>
2023-01-15 21:29:35 +0200

    feat: fix cluster module

--- src/cluster.ts
+++ src/cluster.ts
@@ -2,7 +2,7 @@ import http from 'http';
 import cluster from 'node:cluster';
 import os from 'os';
 import dotenv from 'dotenv';
-import getServerInstance from "./server";
+import getServerInstance from './server';
 
 dotenv.config();
 
@@ -12,35 +12,37 @@ const startingPort = Number(process.env.PORT);
 let currentWorkerIndex = 0;
 let worker;
 
-const loadBalancer = http.createServer((req, res) => {
-
-  const proxyReq = http.request({
-    host: 'localhost',
-    port: startingPort + currentWorkerIndex,
-    path: req.url,
-    method: req.method,
-    headers: req.headers,
-  }, (proxyRes) => {
-    proxyRes.pipe(res);
-  });
-  req.pipe(proxyReq);
-  currentWorkerIndex = (currentWorkerIndex + 1) % numCores;
-
-  if (cluster.isPrimary) {
-    for (let i = 0; i < numCores; +1) {
-      cluster.fork();
-    }
-  } else {
-    const server = getServerInstance();
-    worker = cluster.workers ? cluster.workers[currentWorkerIndex] : undefined;
-    server.listen(startingPort + currentWorkerIndex, () => {
-      console.log(`Server listening by worker - ${worker.id}`);
-    });
+if (cluster.isPrimary) {
+  for (let i = 0; i < numCores; i += 1) {
+    cluster.fork();
   }
-});
 
-loadBalancer.listen(startingPort, () => {
-  console.log(`Load balancer started on: ${startingPort}`);
-});
+  const loadBalancer = http.createServer((req, res) => {
+    const proxyReq = http.request({
+      host: 'localhost',
+      port: startingPort + currentWorkerIndex,
+      path: req.url,
+      method: req.method,
+      headers: req.headers,
+    }, (proxyRes) => {
+      proxyRes.pipe(res);
+    });
+    req.pipe(proxyReq);
+    currentWorkerIndex = (currentWorkerIndex + 1) % numCores;
+  });
 
+  loadBalancer.listen(startingPort, () => {
+    console.log(`Load balancer started on: ${startingPort}`);
+  });
+} else {
+  let workerId;
+  if (cluster.worker) {
+    workerId = cluster.worker.id;
+  }
 
+  const server = getServerInstance(`Request is caught by the worker - ${workerId}`);
+
+  server.listen(startingPort + workerId, () => {
+    console.log(`Worker is listening port: ${startingPort + workerId}`);
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import { createServer, IncomingMessage, ServerResponse } from 'http';
 import requestHandler from './controllers/RequestHandler';
 import { IncomingBody } from './types';
 
-export default function getServerInstance() {
+export default function getServerInstance(workerMessage?: string) {
   return createServer((req: IncomingMessage & IncomingBody, res: ServerResponse) => {
+    if (workerMessage) {
+      console.log(workerMessage);
+    }
+
     requestHandler(req, res)
       .catch((error) => {
         console.error(error);
